21fcbf8d9092fa5930dcd5192a909e84e77486b8
SHRIPADA <shripada@robosoftin.com>
2019-08-24 10:36:38 +0530

    fail0

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+const Warning = React.lazy(() => import('./warning'))
+const a = 10;
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1> Hello World New from WEBPACK - hot</h1>
+        <h2 className={this.state.count > 10 ? 'warning' : null}>
+          {' '}
+          Count: {this.state.count}
+          {this.state.count > 10 ? (
+            <React.Suspense fallback={null}>
+              <Warning />
+            </React.Suspense>
+          ) : null}
+        </h2>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+      </React.Fragment>
+    )
+  }
+}
+
+const hotFunction = hot(module)
+export default hotFunction(App)
