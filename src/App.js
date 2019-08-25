af1f921f766c821b8c10097687f2426cfcd51c89
SHRIPADA <shripada@robosoftin.com>
2019-08-25 16:00:21 +0530

    Cleanup for a boiler plate

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
-const Warning = React.lazy(() => import('./warning'))
 
 class App extends React.Component {
-  state = {
-    count: 0
-  }
-
   render() {
     return (
       <React.Fragment>
-        <h1> Hello World New from WEBPACK - hot</h1>
-        <h2 className={this.state.count > 10 ? 'warning' : null}>
-          {' '}
-          Count: {this.state.count}
-          {this.state.count > 10 ? (
-            <React.Suspense fallback={null}>
-              <Warning />
-            </React.Suspense>
-          ) : null}
-        </h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
+        <h1> Hello World</h1>
       </React.Fragment>
     )
   }
--- src/warning.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-const Warning = () => <span className={'warning'}>Take it easy </span>
-export default Warning
