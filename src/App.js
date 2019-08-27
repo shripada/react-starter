d6e38f1a04137b210405a92068d9326693f337c1
SHRIPADA <shripada@robosoftin.com>
2019-08-27 11:08:34 +0530

    Added support for snapshot testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1> Hello World</h1>
+        <h1> Hello World there it is</h1>
       </React.Fragment>
     )
   }
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,10 +1,17 @@
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import { render } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+
 import App from './App'
 
 describe('App', () => {
   it('Renders without error', () => {
     render(<App />)
   })
+})
+
+it('renders correctly', () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
